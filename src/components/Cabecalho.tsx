0d0977d2d70c3ffb82d4c6389cfc7ecef31bf016
joaopedro-style <joaopedro18231@hotmail.com>
2025-07-25 11:47:28 -0300

    Cria e organiza os componentes

---
+++ src/components/Cabecalho.tsx
@@ -0,0 +1,14 @@
+export default function Cabecalho() {
+  return (
+    <header>
+      <h1>Exercício React 🤔</h1>
+      <hr />
+      <nav>
+        <a href="">Home</a>
+        <a href="">Back End</a>
+        <a href="">PHP</a>
+        <a href="">MYSQL</a>
+      </nav>
+    </header>
+  );
+}
