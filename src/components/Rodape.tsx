8af68d9a38a78203ebb6a459bc4df50f03e58bfc
joaopedro-style <joaopedro18231@hotmail.com>
2025-07-27 12:23:55 -0300

    Aplica css

--- src/components/Rodape.tsx
+++ src/components/Rodape.tsx
@@ -1,10 +1,24 @@
+import type { CSSProperties } from "react";
+
+const estilosRodape: CSSProperties = {
+  backgroundClip: "#333",
+  color: "white",
+  textAlign: "center",
+  padding: "1rem",
+};
+
 export default function Rodape() {
   return (
-    <footer>
-      <h2>Pequeno Projeto React</h2>
-      <p>
-        Desenvolvido por <b>João Pedro</b> &copy; 2025
-      </p>
-    </footer>
+    <>
+      <footer style={estilosRodape}>
+        <h2>Pequeno Projeto React</h2>
+        <p style={{ color: "orange", fontWeight: "bold" }}>
+          Desenvolvido por <b>João Pedro</b> &copy; 2025
+        </p>
+      </footer>
+      <hr />
+      <p className="outro-texto">Praticando...</p>
+      <hr />
+    </>
   );
 }
